aa786e241d61ef80ed421e6a18214e527975f45b
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-12 21:38:28 -0300

    corrigindo loggings

--- config/loggings.py
+++ config/loggings.py
@@ -3,5 +3,6 @@ logging.basicConfig(
     level=logging.INFO , 
     format="%(asctime)s - %(levelname)s - %(message)s" , 
     filemode='w' ,
-    filename='logs/pipeline.logs'
+    filename='logs/pipeline.logs', 
+    encoding='utf-8'
 )
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.data_import.data_import import DataImport 
 from config.data_dict import data_sources_dict
+from config.loggings import logging
 def main(): 
     importacao = DataImport(data_sources_dict)
     importar = importacao.importar_dados()
