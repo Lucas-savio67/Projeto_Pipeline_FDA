74753a4c8ef26e36fdac2c36919a8d2ec3a04c2f
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-12 21:34:45 -0300

    add loggins ao modulo de importacao

--- src/data_import/data_import.py
+++ src/data_import/data_import.py
@@ -9,9 +9,12 @@ class DataImport:
     def importar_dados(self) -> str : 
         apis = self.data.get('apis', [])
         self.importar_dados_apis(apis)
+        return self.imported_data
     def importar_dados_apis(self, apis:dict[str, Any]) -> dict[str, Any] : 
         imported_apis = {}
+        api_errors = {}
         if apis :
+            logger.info("A importação de APIs começou! ")
             for nome_api,info in apis.items(): 
                     if info['params']: 
                         response = requests.get(info['url'], info['params'], timeout=10)
@@ -19,12 +22,19 @@ class DataImport:
                         response = requests.get(info['url'], timeout=10)
                     if response.status_code != 200 : 
                         logger.warning(f"Erro, a API {nome_api} falhou, code: {response.status_code}")
+                        api_errors[nome_api] = {'status_code': response.status_code}
                         continue 
                     else : 
                         data = response.json()
                         logger.info(f"A API {nome_api} foi importada com sucesso! ")
                         imported_apis[nome_api] = data
                         self.imported_data['apis'] = imported_apis
+            logger.info("A importação de APIs terminou! ")
+            logger.info(f"APIs importadas com êxito: {list(imported_apis.keys())}")
+            if api_errors : 
+                logger.info(f"APIs que falharam: {api_errors}")
             return self.imported_data
         else : 
+            logger.info('Não há APIs para importar, pulando este módulo...')
             return 'Sem APIs para importar! '
+            
