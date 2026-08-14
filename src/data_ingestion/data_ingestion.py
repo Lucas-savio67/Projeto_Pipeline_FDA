8e364c359ee6541cde19b90037ac3739fcbb56f6
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-14 17:52:50 -0300

    add modulo de ingestao na pasta data

---
+++ src/data_ingestion/data_ingestion.py
@@ -0,0 +1,34 @@
+import logging 
+import json 
+logger = logging.getLogger(__name__)
+from typing import Any 
+class IngestionErrors(Exception): 
+    pass
+class DataIngestion : 
+    def __init__(self, imported_data:dict[str, dict[str,Any]]) -> None : 
+        self.imported_data = imported_data 
+    def injetar_dados(self) -> str : 
+        ingested_data = {}
+        if not self.imported_data : 
+            logger.error("Erro, nenhum dado foi importado, o dicionário está vazio! ")
+            raise IngestionErrors("Erro, nenhum dado foi importado, o dicionário está vazio! ")
+        logger.info("Começando o fluxo  de ingestão dos dados! ")
+        apis = self.imported_data.get('apis', [])
+        data =self.injetar_dado_apis(apis)
+        ingested_data['dados_injetados'] = data
+        logger.info("O fluxo de ingestão terminou")
+        logger.info(f"Dados injetados com sucesso: {ingested_data}")
+
+    def injetar_dado_apis(self, apis:dict[str,Any]) -> str : 
+        ingested_apis = {}
+        if not apis : 
+            logger.warning("O tipo de dado API não foi encontrado! ")
+        logger.info("Começando o fluxo de ingestão das APIs...")
+        for nome_api, api in apis :
+            logger.info(f"Injetando a APi {nome_api} no diretório data! ")
+            output_file = f'data/{nome_api}.json'
+            with open(output_file, 'w') as f : 
+                json.dump(api, f) 
+            ingested_apis['apis'] = nome_api
+            logger.info(f"A API {nome_api} foi carregada no diretório data com sucesso! ")
+        return ingested_apis
