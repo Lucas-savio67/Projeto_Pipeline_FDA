ee46ab06b40884b85146d7b1549ac365cfad53db
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-15 14:38:26 -0300

    colocando extracao e ingestao no arquivo orquestrador

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.data_import.data_import import DataImport 
+from src.data_ingestion.data_ingestion import DataIngestion
+from src.data_extraction.data_extraction import DataExtraction
 from config.data_dict import data_source_dict
 from config.loggings import logging
 def main(): 
     data_sources = data_source_dict()
     importacao = DataImport(data_sources)
     importar = importacao.importar_apis()
-    print(importar)
+    ingestao = DataIngestion(importar)
+    injetar = ingestao.injetar_dados()
+    extracao = DataExtraction()
+    extrair = extracao.extrair_dados()
+    print(extrair)
 print(main())
