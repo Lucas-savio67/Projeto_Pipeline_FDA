37619eede13788f440275b03c321ff4e9317833e
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-15 19:26:36 -0300

    add tratamento de erros para extracao

--- src/data_extraction/data_extraction.py
+++ src/data_extraction/data_extraction.py
@@ -21,7 +21,11 @@ class DataExtraction :
                     api_data = self.extrair_dados_apis('data/bronze/apis/')
                     self.extracted_data['apis'] = api_data
             except ExtractionErrors as e : 
-                logger.warning(e)
+                logger.warning(str(e))
+            except json.JSONDecodeError as e: 
+                logger.warning(str(e))
+            except PermissionError as e : 
+                logger.warning(str(e))
         logger.info("O fluxo de extracão terminou! ")
         logger.info(f"Dados extraídos: {self.extracted_data}")
         return self.extracted_data
@@ -31,12 +35,20 @@ class DataExtraction :
         if not diretorio.exists(): 
             logger.warning("Nenhuma API foi encontrada! ")
             raise ExtractionErrors("Nenhuma API foi encontrada! ")
-        for arquivo in diretorio.iterdir(): 
-            if arquivo.is_file(): 
-                nome_api = arquivo.name.split('.json')
-                caminho_arquivo = f'{arquivo}' 
-                with open(caminho_arquivo, 'r', encoding='utf-8') as f : 
-                    api_data = json.load(f)
-                    extracted_apis[nome_api[0]] = api_data
-                logger.info(f"O arquivo {arquivo} foi extraído com sucesso! ")
+        try :
+            for arquivo in diretorio.iterdir(): 
+                if arquivo.is_file(): 
+                    nome_api = arquivo.name.split('.json')
+                    caminho_arquivo = f'{arquivo}' 
+                    try :
+                        with open(caminho_arquivo, 'r', encoding='utf-8') as f : 
+                            api_data = json.load(f)
+                            extracted_apis[nome_api[0]] = api_data
+                        logger.info(f"O arquivo {arquivo} foi extraído com sucesso! ")
+                    except json.JSONDecodeError as e : 
+                        logger.warning(f"Erro {e}, o JSON {nome_api} é inválido! ")
+                    except PermissionError as e : 
+                        logger.warning(f"Erro {e}, permissão de leitura negada para a API {nome_api}! ")
+        except NotADirectoryError as e : 
+            raise ExtractionErrors("Erro, o caminho fornecido não é um diretório! ") 
         return extracted_apis
