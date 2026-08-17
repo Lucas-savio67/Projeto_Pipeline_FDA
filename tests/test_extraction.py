cfacced7458b35140261cd44d061bcff9f95697a
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-16 21:33:24 -0300

    add test de extracao com JSON inválido

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -2,9 +2,10 @@ import logging
 import pytest
 import json
 import io
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import patch, MagicMock
 from src.data_extraction.data_extraction import DataExtraction, ExtractionErrors
 from botocore.exceptions import ClientError
+from json import JSONDecodeError
 def test_success_extraction(): 
     dados_teste={'campo_1':'valor_1'}
     mock_client = MagicMock()
@@ -26,5 +27,15 @@ def test_client_error():
     operation_name='PutObject'
 ) 
     with pytest.raises(ExtractionErrors) : 
+        extracao = DataExtraction(mock_client, 'bucket_teste')
+        extrair = extracao.extrair_dados_apis('key_teste')
+@patch('src.data_extraction.data_extraction.json.loads')
+def test_invalid_json(mock_load): 
+    dados_teste={'campo_1':'valor_1'}
+    mock_client = MagicMock()
+    mock_load.side_effect = JSONDecodeError(   msg="Expecting value",
+    doc="{invalido",
+    pos=1)
+    with pytest.raises(ExtractionErrors): 
         extracao = DataExtraction(mock_client, 'bucket_teste')
         extrair = extracao.extrair_dados_apis('key_teste')
