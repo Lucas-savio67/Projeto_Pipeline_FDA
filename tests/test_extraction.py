7f5a6fabf2c651e223deabd83129c43cf6dbc5f5
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-16 21:21:10 -0300

    add teste de client error

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -4,7 +4,7 @@ import json
 import io
 from unittest.mock import Mock, patch, MagicMock
 from src.data_extraction.data_extraction import DataExtraction, ExtractionErrors
-
+from botocore.exceptions import ClientError
 def test_success_extraction(): 
     dados_teste={'campo_1':'valor_1'}
     mock_client = MagicMock()
@@ -15,3 +15,16 @@ def test_success_extraction():
     extracao = DataExtraction(mock_client, 'bucket_teste')
     extrair = extracao.extrair_dados_apis('key_teste')
     assert extrair == {'key_teste': dados_teste}
+def test_client_error(): 
+    mock_client = MagicMock() 
+    mock_client.get_object.side_effect = ClientError(  error_response={
+        'Error': {
+            'Code': 'AccessDenied',
+            'Message': 'Access Denied'
+        }
+    },
+    operation_name='PutObject'
+) 
+    with pytest.raises(ExtractionErrors) : 
+        extracao = DataExtraction(mock_client, 'bucket_teste')
+        extrair = extracao.extrair_dados_apis('key_teste')
