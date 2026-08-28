5b77bb49be40418a9baa1bb7a369dceb25b2b39c
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-28 17:21:29 -0300

    alterando nome do arquivo para estruturacao

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import boto3
 from src.data_import.data_import import DataImport 
 from src.data_ingestion.data_ingestion import DataIngestion
 from src.data_extraction.data_extraction import DataExtraction
-from src.data_transforming.data_transforming import  DataStructuring, StructuringErrors
+from src.data_structuring.data_structuring import  DataStructuring, StructuringErrors
 from src.eda.eda import EDA
 from config.data_dict import data_source_dict
 from config.loggings import logging
@@ -22,9 +22,9 @@ def main():
         injetar = ingestao.injetar_dados()
         extracao = DataExtraction(s3_client, info_s3['bucket'])
         extrair = extracao.extrair_dados()
-        transformacao = DataStructuring(extrair, regras_limpeza)
-        transformar = transformacao.transformar_dados()
-        eda = EDA(transformar) 
+        estruturacao = DataStructuring(extrair, regras_limpeza)
+        estruturar = estruturacao.estruturar_dados()
+        eda = EDA(estruturar) 
         fazer_eda = eda.fazer_eda_tabelas()
         print(fazer_eda)
     except LoadingErrors as e : 
