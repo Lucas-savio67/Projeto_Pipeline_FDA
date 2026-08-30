0c2502629ca098e513286b3db704f28f3333d74a
Lucas-savio67 <ccr.lsavio.ti@gmail.com>
2026-08-30 16:10:36 -0300

    add teste de estruturação de tabelas a parte com sucesso

--- tests/test_structuring.py
+++ tests/test_structuring.py
@@ -29,3 +29,32 @@ def test_success_main_structuring():
     assert list(estruturar[0].keys()) == ['eventos']
     assert estruturar[0]['eventos'].to_dict(orient='records') == [{'results_1': 'result'}]
     assert estruturar[1] == {}
+def test_success_separate_table_structuring(): 
+    api = {'meta': 'metadata','results': [{'safetyreportid': 'id' ,'results_1':'result' , 'drug': [{'drug_1': 'drugs'}] , 'reaction': [{'reaction_1': 'reactions'}]}]}
+    regras_limpeza = { 
+        'apis': { 
+            'FDA_DRUG': { 
+                'nome_tabela_principal': 'eventos' , 
+                'local_registros': 'results',
+                'tabelas_a_parte': { 
+                    'reaction': { 
+                        'record_path': ['patient', 'reaction'], 
+                        'meta': ['safetyreportid']
+                    } , 
+                    'drug': { 
+                        'record_path': ['patient','drug'] ,
+                        'meta': ['safetyreportid'] 
+                    }
+    
+                    }
+                } 
+            }
+        }
+    regra_api = regras_limpeza['apis']['FDA_DRUG']
+    estruturacao = DataStructuring(api ,regras_limpeza ) 
+
+    estruturar = estruturacao.criar_tabelas_novas(api , regra_api)
+    assert estruturar[0] == {}
+    assert set(estruturar[1].keys()) == {'reaction', 'drug'}
+    assert estruturar[1]['reaction']['erro'] == "'patient'"
+    assert estruturar[1]['drug']['erro'] == "'patient'"
